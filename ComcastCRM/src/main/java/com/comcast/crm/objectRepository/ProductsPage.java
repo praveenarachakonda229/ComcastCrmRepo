05c574fdc3d35091d7bf21151aeae7e5eb0844f0
Praveena Rachakonda <praveenarachakonda229@gmail.com>
2026-08-24 16:14:44 +0530

    Added new element in products page

--- ComcastCRM/src/main/java/com/comcast/crm/objectRepository/ProductsPage.java
+++ ComcastCRM/src/main/java/com/comcast/crm/objectRepository/ProductsPage.java
@@ -6,4 +6,7 @@ import org.openqa.selenium.support.FindBy;
 public class ProductsPage {
 	@FindBy(xpath="//input[@alt='Create Product']")
 	private WebElement createProductImgBtn;
+	
+	@FindBy(name="search_text")
+	private WebElement ele2;
 }
